aee526ccfc37ff114c82abefdb12c96ead5a7cc6
EceNarin <la.crymosa321@gmail.com>
2023-02-01 20:39:28 +0300

    add

--- src/test/java/automationExercise/PostToSearchProduct200.java
+++ src/test/java/automationExercise/PostToSearchProduct200.java
@@ -1,7 +1,11 @@
 package automationExercise;
 
 import baseUrls.ExerciseBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class PostToSearchProduct200 extends ExerciseBaseUrl {
     @Test
@@ -13,6 +17,16 @@ Request Parameter: search_product (For example: top, tshirt, jean)
 Response Code: 200
 Response JSON: Searched products list
          */
-
+      specExercise.pathParam("pp1","searchProduct").queryParam("search_product","jean");
+        Response response=given()
+                .spec(specExercise)
+                .contentType("application/json")
+                //.accept("text/html; charset=utf-8")
+                .when()
+                .post("/{pp1}");
+        response.prettyPrint();
+        System.out.println(response.contentType());
+        response.then()
+                .assertThat().statusCode(200);
     }
 }
